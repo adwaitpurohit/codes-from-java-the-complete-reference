df369016cce129d495900b28c4fab160ac72190b
purohitaj@gmail.com <purohitaj@gmail.com@a6a6f7ba-7b27-8516-d744-3c838cd91661>
2011-10-17 07:33:07 +0000

    Committing edited chapter 2 codes.

--- trunk/Eclipse Workspace/Code/src/chapter2/ForTest.java
+++ trunk/Eclipse Workspace/Code/src/chapter2/ForTest.java
@@ -1,9 +1,13 @@
 package chapter2;
 
-public class ForTest {
-	public static void main(String[] args) {
+/*
+ Demonstrate the for loop.
+ Call this file "ForTest.java".
+ */
+class ForTest {
+	public static void main(String args[]) {
 		int x;
 		for (x = 0; x < 10; x = x + 1)
-			System.out.println("This is x : " + x);
+			System.out.println("This is x: " + x);
 	}
 }
