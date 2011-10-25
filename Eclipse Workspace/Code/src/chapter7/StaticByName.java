5a71594ca2de9eabd0984b9cfa26e635ed0500c2
purohitaj@gmail.com <purohitaj@gmail.com@a6a6f7ba-7b27-8516-d744-3c838cd91661>
2011-10-25 11:45:32 +0000

    Committing some chapter 7 codes.

---
+++ Eclipse Workspace/Code/src/chapter7/StaticByName.java
@@ -0,0 +1,18 @@
+package chapter7;
+
+class StaticByName {
+
+	static class StaticDemo {
+		static int a = 42;
+		static int b = 99;
+
+		static void callme() {
+			System.out.println("a = " + a);
+		}
+	}
+
+	public static void main(String args[]) {
+		StaticDemo.callme();
+		System.out.println("b = " + StaticDemo.b);
+	}
+}
