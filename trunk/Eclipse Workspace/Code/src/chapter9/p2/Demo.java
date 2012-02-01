b74d5533873348998802096fcb6bcfed44c56866
purohitaj@gmail.com <purohitaj@gmail.com@a6a6f7ba-7b27-8516-d744-3c838cd91661>
2012-02-01 11:21:06 +0000

    Committing all chapter 9 codes.

---
+++ trunk/Eclipse Workspace/Code/src/chapter9/p2/Demo.java
@@ -0,0 +1,11 @@
+package chapter9.p2;
+
+//Instantiate the various classes in p2.
+public class Demo {
+	public static void main(String args[]) {
+		Protection2 ob1 = new Protection2();
+		ob1.getClass();
+		OtherPackage ob2 = new OtherPackage();
+		ob2.getClass();
+	}
+}
