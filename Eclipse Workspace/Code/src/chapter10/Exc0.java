fe82d2bd012de45ca6d75b7ef8200f980a9962a6
purohitaj@gmail.com <purohitaj@gmail.com@a6a6f7ba-7b27-8516-d744-3c838cd91661>
2012-02-07 12:00:03 +0000

    Committing all chapter 10 codes.

---
+++ Eclipse Workspace/Code/src/chapter10/Exc0.java
@@ -0,0 +1,9 @@
+package chapter10;
+
+class Exc0 {
+	public static void main(String args[]) {
+		int d = 0;
+		int a = 42 / d;
+		System.out.println("This will not be printed. - a = " + a);
+	}
+}
---
+++ Eclipse Workspace/Code/src/chapter10/ThrowsDemo.java
@@ -0,0 +1,13 @@
+package chapter10;
+
+//This program contains an error and will not compile.
+class ThrowsDemo {
+	static void throwOne() {
+		System.out.println("Inside throwOne.");
+		//throw new IllegalAccessException("demo");
+	}
+
+	public static void main(String args[]) {
+		throwOne();
+	}
+}
